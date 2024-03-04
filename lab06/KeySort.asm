084306668569bd41a38e809081d7a06b9272f892
rinpmchs <90576981+rinpmchs@users.noreply.github.com>
2024-03-05 01:47:28 +0200

    hw6

--- lab06/KeySort.asm
+++ lab06/KeySort.asm
@@ -44,18 +44,14 @@ input_end:
 
 main:
   read_int(a1)  # n
-  slli t0, a0, 2
+  read_int(a3)  # t   (0 - comp1, 1 - comp2)
+  slli t0, a1, 2
   allocate_memory(t0)
   mv a2, a0  # arr address
 
   jal input_array
-  li a3, 0  # 0 - comp1, 1 - comp2
-  jal function # a1 = n, a2 = arr, a3 - comparator
-  jal output_array
-  li a3, 1
-  jal function
-  jal output_array
-  j end
+  jal function  # a1 = n, a2 = arr, a3 - comparator
+  j output_array
   
 function:
   addi sp, sp, -24
@@ -71,7 +67,6 @@ function:
   mul t4, t4, t1
   add t4, t2, t4  # arr end
   
-  #bgt t0, t1, func_end
   li s3, 0  # tmp
   addi t4, t4, -4
   loop1:
@@ -82,10 +77,10 @@ function:
       lw s1, 0(t2)  # current element
       lw s2, 4(t2)  # next element
       beqz a3, clt
-      jal ra, comparator_ld_greater_than
+      j cgt
     continue:
       mv t3, a0
-      bnez t3, no_swap  # here i will use comparator
+      bnez t3, no_swap  # here i use comparator value
       mv s3, s1
       sw s2, 0(t2)
       sw s3, 4(t2)
@@ -97,9 +92,12 @@ function:
   end_loop2:
     bnez t5, loop1 
     j func_end
-  
+    
   clt:
      jal ra, comparator_less_than
+     j continue
+  cgt:
+     jal ra, comparator_ld_greater_than
      j continue
   
 func_end:
@@ -110,18 +108,18 @@ func_end:
   lw t5, 16(sp)
   lw ra, 20(sp)
   addi sp, sp, 24
-  jr ra
+  jalr ra
   
 comparator_less_than:
   addi sp, sp, -4
   sw s0, 0(sp)
-  blt s1, s2, less
+  ble s1, s2, less
 more:
   li s0, 0  # false
   mv a0, s0
   lw s0, 0(sp)
   addi sp, sp, 4
-  jalr zero, 0(ra)
+  ret
 less:
   li s0, 1  # true
   mv a0, s0
@@ -137,7 +135,7 @@ comparator_ld_greater_than:
   li t2, 10
   rem t1, s1, t2
   rem t2, s2, t2
-  blt t2, t1, less_ld
+  bge t1, t2, less_ld
 more_ld:
   li s0, 0  # false
   mv a0, s0
@@ -168,6 +166,5 @@ output_end:
   li a0, '\n'
   li a7, 11
   ecall
-  ret
   
 end:
